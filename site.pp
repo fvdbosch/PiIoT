e70d39f9cd2a3c9e1e8cfb83479ad14d863b16b7
fvdbosch <f.vdbosch@gmail.com>
2016-05-25 22:02:06 +0200

    Create site.pp

---
+++ site.pp
@@ -0,0 +1,16 @@
+include apt
+
+apt::key { 'openhab':
+  key_source => 'https://bintray.com/user/downloadSubjectPublicKey?username=openhab',
+}
+
+apt::source { 'openhab':
+  location     => 'http://dl.bintray.com/openhab/apt-repo',
+  release      => 'stable',
+  repos        => 'main',
+  include_src  => false,
+}
+
+package { 'openhab-runtime':
+  ensure => 'installed',
+}
