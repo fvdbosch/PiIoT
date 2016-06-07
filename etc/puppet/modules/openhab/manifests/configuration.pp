43270f0a685d929ab6d23df7dfc869f1c476ec51
fvdbosch <f.vdbosch@gmail.com>
2016-06-07 22:34:06 +0200

    development

---
+++ etc/puppet/modules/openhab/manifests/configuration.pp
@@ -0,0 +1,70 @@
+class openhab::configuration (
+  $security       = 'OFF',
+  $enocean        = 'true',
+  $enocean_serial = '/dev/ttyS0',
+  $persistence    = true,
+  $db_name        = 'openhab',
+  $db_user        = 'openhab',
+  $db_pass        = 'openhab',
+) {
+
+  # Bindings
+  if $enocean == true {
+    package {
+      'openhab-addon-binding-enocean':
+        ensure => 'installed',
+        require => Package['openhab-runtime'];
+    }
+  }
+
+  # Persistence
+  if $persistence == true {
+    class { '::mysql::server':
+      root_password           => 'strongpassword',
+      remove_default_accounts => true,
+      override_options        => {
+        'mysqld' => {
+          'max_connections' => '1024',
+        },
+      },
+    } ->
+
+    mysql::db { $db_name:
+      user     => $db_user,
+      password => $db_pass,
+      host     => 'localhost',
+      grant    => [ALL],
+    } ->
+
+    package {
+      'openhab-addon-persistence-mysql':
+        ensure => 'installed',
+        require => Package['mysql-server', 'openhab-runtime'];
+    } ->
+
+    file {
+      '/etc/openhab/configurations/persistence/mysql.persist':
+      source  => 'puppet:///modules/openhab/openhab.persist';
+    }    
+  }
+
+  File {
+    ensure  => 'present',
+    require => Package['openhab-runtime'],
+  }
+
+  file {
+    '/etc/openhab/configurations/openhab.cfg':
+      content => template('openhab/openhab.cfg.erb'),
+      notify  => Service['openhab'];
+    '/etc/openhab/configurations/sitemaps/openhab.sitemap':
+      source  => 'puppet:///modules/openhab/openhab.sitemap';
+    '/etc/openhab/configurations/items/openhab.items':
+      source  => 'puppet:///modules/openhab/openhab.items';
+    '/etc/openhab/configurations/rules/openhab.rules':
+      source  => 'puppet:///modules/openhab/openhab.rules';
+    '/etc/openhab/configurations/transform/openhab.map':
+      source  => 'puppet:///modules/openhab/openhab.map';
+  }
+
+}
--- etc/puppet/modules/openhab/manifests/init.pp
+++ etc/puppet/modules/openhab/manifests/init.pp
@@ -1,49 +1,26 @@
 class openhab {
 
   include apt
+  include openhab::configuration
 
   apt::key { 'openhab':
     key_source => 'https://bintray.com/user/downloadSubjectPublicKey?username=openhab',
-  }
+  } ->
 
   apt::source { 'openhab':
     location     => 'http://dl.bintray.com/openhab/apt-repo',
     release      => 'stable',
     repos        => 'main',
     include_src  => false,
+  } ->
+
+  package { 'openhab-runtime':
+    ensure  => 'installed',
+  } ~>
+
+  service { 'openhab':
+    ensure => 'running',
+    enable => true,
   }
 
-  package {
-    'openhab-runtime':
-      ensure => 'installed';
-    'openhab-addon-binding-enocean':
-      ensure => 'installed',
-      require => Package['openhab-runtime'];
-    'openhab-addon-binding-hue':
-      ensure => 'installed',
-      require => Package['openhab-runtime'];
-    'openhab-addon-persistence-mysql':
-      ensure => 'installed',
-      require => Package['openhab-runtime'];
-  }
-
-  File {
-    ensure  => 'present',
-    require => Package['openhab-runtime'],
-  }
-
-  file {
-    '/etc/openhab/configurations/openhab.cfg':
-      source => 'puppet:///modules/openhab/config';
-    '/etc/openhab/configurations/sitemaps/openhab.sitemap':
-      source  => 'puppet:///modules/openhab/sitemap';
-    '/etc/openhab/configurations/items/openhab.items':
-      source  => 'puppet:///modules/openhab/items';
-    '/etc/openhab/configurations/rules/openhab.rules':
-      source  => 'puppet:///modules/openhab/rules';
-    '/etc/openhab/configurations/transform/openhab.transform':
-      source  => 'puppet:///modules/openhab/transform';
-    '/etc/openhab/configurations/persistence/openhab.persistence':
-      source  => 'puppet:///modules/openhab/persistence';
-  }
 }
